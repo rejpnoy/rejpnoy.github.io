c0544cb612e9f1d390e25061cbeab8df4cd53e9d
rejpnoy <30378137+rejpnoy@users.noreply.github.com>
2021-07-19 20:07:43 -0400

    Update addNumbers.py

--- addNumbers.py
+++ addNumbers.py
@@ -11,3 +11,16 @@ def add(n1, n2):
   addSum = x + y
   print((type(addSum)) ,  " " , addSum )
   return 
+def add(n1, n2):
+  #verify first the type of the variable it is 
+  print((type(n1)) ,  " " , n1 )
+  print(type(n2) ,  " " , n2)
+  #now change the variables to a int 
+  x = int(n1)
+  y = int(n2)
+  print((type(x)) ,  " " , x )
+  print((type(y)) ,  " " , y )
+  #now add them  
+  addSum = x + y
+  print((type(addSum)) ,  " " , addSum )
+  return 
