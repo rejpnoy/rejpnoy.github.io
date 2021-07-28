7eb00fc91649506d79204968cfa691d45d9d74e3
rejpnoy <30378137+rejpnoy@users.noreply.github.com>
2021-07-28 17:07:53 -0400

    Update sum_odd_and_even.py

--- sum_odd_and_even.py
+++ sum_odd_and_even.py
@@ -1,33 +1,6 @@
 lst1 = [1, 2, 3, 4, 5, 6]
-lst = [-1, -2, -3, -4, -5, -6]
-x = 42
-def sum_odd_and_even(lst):
-    print("HELLO")
-    length  = len(lst)
-    for i in lst:
-        if(i % 2 == 0):
-            addSum = i
-            print(str(i) + " EVEN")
-            addSum = sum(length)
-        
-            # Now ADD IT 
-            
-           # addSumEven = int(i)
-            #addSumEven = sum(i)
-         
-           # print(addSumEven, "TOTAL SUM")
-        else:
-            print(type(i))
-            print(str(i) + " ODD")
-            
-
-           # addSumOdd = int(i)
-           # addSumOdd = sum(i)
-           # print(addSumOdd, "TOTAL SUM")
-        
-  
-
-    return 
+lst2 = [-1, -2, -3, -4, -5, -6]
+lst3 = [0, 0]
 
 def sum_odd_and_even1(lst):
     odd_sum = 0 
@@ -37,7 +10,9 @@ def sum_odd_and_even1(lst):
         
         print(type(lst))
         if(i % 2 ==  0):
+            print(type(i))
             print(str(i) + " This is an EVEN Number")
+            print(type(even_sum))
             even_sum = even_sum + i        
         else:
             print(str(i) + " This is an EVEN Number")
