6ac6e428658c34060a52b478f84fb0871981174b
rejpnoy <30378137+rejpnoy@users.noreply.github.com>
2021-07-28 12:42:17 -0400

    Update sum_odd_and_even.py

--- sum_odd_and_even.py
+++ sum_odd_and_even.py
@@ -1,12 +1,14 @@
 lst = [1, 2, 3, 4, 5, 6]
-
+x = 42
 def sum_odd_and_even(lst):
     print("HELLO")
     print(lst)
-    if(lst % 2) == 0:
-       print("{0} is Even".format(lst))
-    else:
-       print("{0} is Odd".format(lst))
+    for i  in range(lst):
+        print(list[i])
+        if(i % 2) == 0:
+            print("{0} is Even".format(i))
+        else:
+            print("{0} is Odd".format(i))
 
     return 
 
