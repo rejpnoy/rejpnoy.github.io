2fef907f8117bbcc9a83d9eb46ecddc1801009f0
renriqu1 <renriqu1@masterdevlulz.com>
2022-03-22 07:00:26 -0400

    adding  indexttest python

---
+++ indextest.py
@@ -0,0 +1,12 @@
+name = "bro code"
+
+if(name[0]).islower()):
+	name = name.capitalize()
+
+print(name)
+# index operator
+
+#substrings fromt eh first part of the name 
+first_name = name[0:3].upper()
+
+print(first_name) 
