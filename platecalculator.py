c62bd8175d8d05d719abb5c52a649136bd127ab7
rejpnoy <30378137+rejpnoy@users.noreply.github.com>
2022-11-10 15:34:34 -0500

    Update platecalculator.py
    for loop addition of plates

--- platecalculator.py
+++ platecalculator.py
@@ -1,27 +1,31 @@
-# This will calculate the amount of weights in a bar for lifting 
-#Rommel Enriquez 
+# This will calculate the amount of weights in a bar for lifting
+# Rommel Enriquez
 
 print("Your name?")
 lifter = input()
 print("Lifter's name: " + lifter.upper())
 
-
 def plates():
-    #inputs in the list of what the lifter puts in. and x times that by 2 
+    # inputs in the list of what the lifter puts in. and x times that by 2
     print("how heavy is the bar?: ")
-    barweight = int(input())
-    if barweight == 35:
-        print("How many plates on 1 side?: ")
-        plates_list = []
-        n = int(input("How many plates on 1 side?: "))
+    bar_weight = int(input())
+    if bar_weight == 35:
 
-        
+        plates_list = []
+
+        n = int(input("How many plates on 1 side?: "))
+        print("What is the weight per pounds for each plates: ")
 
         for i in range(0, n):
-            ele = int(input())
-            plates_list.append(ele)
 
-        
+            ele = int(input())
+            # adds the plated input into the list of elements
+            plates_list.append(ele)
+            # gives me the total from the plates_list
+        print("total of the plates: ")
+        print(sum(plates_list))
     return plates_list
+
+
 
 print(plates())
